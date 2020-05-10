b6bb4e2543d523586ca314e71d8a12eaf6345d66
Nii-code <niidarku66@gmail.com>
2020-05-10 21:28:07 +0000

    continued ..

--- MyPersonalPro/urls.py
+++ MyPersonalPro/urls.py
@@ -21,3 +21,4 @@ urlpatterns = [
     path('', include('PersonalPRO.urls')),
     path('admin/', admin.site.urls),
 ]
+
--- PersonalPRO/views.py
+++ PersonalPRO/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 
 # Create your views here.
 def post_list(request):
     return render(request, 'PersonalPRO/post_list.html')
 
+
+class AboutView(TemplateView):
+    template_name = 'About.html'
